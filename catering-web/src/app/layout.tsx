ce6cef5490f5587fd9f240d99b2816dbdea58ca2
SuzuyaBish <sloan.jarrod@gmail.com>
2023-11-21 14:28:59 +0200

    fix seo open graph

--- catering-web/src/app/layout.tsx
+++ catering-web/src/app/layout.tsx
@@ -36,28 +36,12 @@ export const metadata: Metadata = {
     },
   ],
   creator: "Jarrod Sloan",
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-    // images: [
-    //   {
-    //     url: siteConfig.ogImage,
-    //     width: 1200,
-    //     height: 630,
-    //     alt: siteConfig.name,
-    //   },
-    // ],
-  },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
     // images: [siteConfig.ogImage],
-    creator: "@shadcn",
+    // creator: "@shadcn",
   },
   icons: {
     icon: "/favicon.ico",
