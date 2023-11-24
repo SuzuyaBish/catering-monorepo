cfb2d738a295a4890cf9db939fabc55c282ec447
SuzuyaBish <sloan.jarrod@gmail.com>
2023-11-24 16:01:08 +0200

    attempt to fix deployment

--- apps/catering-cms/src/app/layout.tsx
+++ apps/catering-cms/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/providers/ThemeProvider"
+import { Toaster } from "sonner"
 
 import { cn } from "@/lib/utils"
-import { Toaster } from "sonner"
 import { SiteFooter } from "@/components/layout/SiteFooter"
 import { TailwindIndicator } from "@/components/TailwindIndicator"
 
